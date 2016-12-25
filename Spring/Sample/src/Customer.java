dc3760318e080cb26ff20f3fd6c7c1ff51998509
Tester <Tester@email.com>
2016-12-25 21:03:30 +0530

    Spring Normal way to create object.

---
+++ Spring/Sample/src/Customer.java
@@ -0,0 +1,20 @@
+
+public class Customer {
+
+	String fname;
+	String email;
+	public String getFname() {
+		return fname;
+	}
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
---
+++ Spring/Sample/src/CustomerManaget.java
@@ -0,0 +1,15 @@
+
+public class CustomerManaget {
+
+	
+	public static void main(String[] args) {
+		
+		 Customer c = new Customer();
+		 c.setFname("Abc");
+		 c.setEmail("abc@email.com");
+		 
+		 //1.We have to create an obje expl
+		 //2.To modify the file we have java code.
+	}
+
+}
